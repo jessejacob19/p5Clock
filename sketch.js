cc38008e2a987b73e26ef6bb2b57f7eb9cb9db47
jesse collier <jesse.collier97@gmail.com>
2019-06-19 20:17:01 +1200

    made the clock move depending where the mouse is

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -12,8 +13,13 @@ function draw() {
   // fill(255);
   // noStroke();
   // text(hr + ":" + mn + ":" + sc, 10, 200);
-  strokeWeight(4);
+  strokeWeight(8);
   stroke(255);
   noFill();
   ellipse(200, 200, 300, 300);
+
+  strokeWeight(4);
+  stroke(255, 100, 150);
+  let end = map(mouseX, 0, width, 0, 360);
+  arc(200, 200, 300, 300, 0, end);
 }
