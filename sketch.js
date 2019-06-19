42325b02d4de0bd70176a991eef8fbc683ef788e
jesse collier <jesse.collier97@gmail.com>
2019-06-19 20:11:58 +1200

    shows a circle

--- sketch.js
+++ sketch.js
@@ -9,7 +9,11 @@ function draw() {
   let mn = minute();
   let sc = second();
 
-  fill(255);
-  noStroke();
-  text(hr + ":" + mn + ":" + sc, 10, 200);
+  // fill(255);
+  // noStroke();
+  // text(hr + ":" + mn + ":" + sc, 10, 200);
+  strokeWeight(4);
+  stroke(255);
+  noFill();
+  ellipse(200, 200, 300, 300);
 }
